b23736d40a4b435c210cee3c7bbc448fb19af5aa
Thai Pangsakulyanont <org.yi.dttvb@gmail.com>
2013-07-12 12:35:08 +0700

    add test to xcode

---
+++ Tests/main.cpp
@@ -0,0 +1,17 @@
+#include <igloo/igloo_alt.h>
+using namespace igloo;
+
+Describe(test_igloo)
+{
+	
+	It(works)
+	{
+		
+	}
+	
+};
+
+int main(int argc, const char **argv)
+{
+	return TestRunner::RunAllTests(argc, argv);
+}
